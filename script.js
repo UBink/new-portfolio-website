7e6ac7ba923d5572761b7df9ded8516b5e3a1920
UBink <140355454+UBink@users.noreply.github.com>
2023-09-26 12:20:11 -0700

    night mode added

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function toggleMenu(){
     const icon = document.querySelector("#dropdown-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-
-
 }
+
+function nightMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+  }
