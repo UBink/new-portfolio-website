7f44f2ba5ff67ead84366a788b9e0ee807b0a6d4
UBink <140355454+UBink@users.noreply.github.com>
2023-09-27 08:09:03 -0700

    update on night mode

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function toggleMenu(){
 
 function nightMode() {
     var element = document.body;
+    const text = document.querySelector('p').style.color;
     element.classList.toggle("dark-mode");
-    
+    if (text === "white") {
+        document.querySelectorAll('p').forEach(e => e.style.color = "rgb(53, 53, 53)");
+    }
+    else {
+        document.querySelectorAll('p').forEach(e => e.style.color = "white");
+    }
   }
