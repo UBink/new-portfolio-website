1014400c4de4ae97af59d63222dfb943dd946e3b
UBink <140355454+UBink@users.noreply.github.com>
2024-12-25 20:55:17 -0800

    fixed bug, updated about me

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ function nightMode() {
     element.classList.toggle("dark-mode");
     if (text === "white") {
         document.querySelectorAll('p').forEach(e => e.style.color = "rgb(53, 53, 53)");
+        document.querySelectorAll('a').forEach(e => e.style.color = "rgb(53, 53, 53)");
+        document.querySelector("#menu-links").style.backgroundColor = "white";
+        document.querySelectorAll("#contact-info-upper-container").backgroundColor = "rgb(250, 250, 250)";
     }
     else {
         document.querySelectorAll('p').forEach(e => e.style.color = "white");
+        document.querySelectorAll('a').forEach(e => e.style.color = "white");
+        document.querySelector("#menu-links").style.backgroundColor = "rgb(44, 43, 46)";
+        document.querySelectorAll("#contact-info-upper-container").backgroundColor = "rgb(44, 43, 46)";
     }
   }
